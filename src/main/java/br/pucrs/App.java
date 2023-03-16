871b47579e96125eec839be79f5b97fe85db9722
Edson Ricardo Da Costa <20413677@pucrs.br>
2023-03-16 18:54:31 -0300

    Initial Commit

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,81 +1,54 @@
 package br.pucrs;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
-        int[] arr = { 70, 50, 30, 10, 20, 40, 60 };
-
-		int[] merged = mergeSort(arr, 0, arr.length - 1);
-
-		for (int val : merged) {
-			System.out.print(val + " ");
-		}
-
+	public static void main(String[] args) {
+		long[] A = geraVetor(16, 16);
+		long[] B = geraVetor(1024, 1024);
+		long[] C = geraVetor(524288, 524288);
+		System.out.println("N=32: " + maxVal1(A, A.length));
+		System.out.println("N=2048: " + maxVal1(B, B.length));
+		System.out.println("N=1048576: " + maxVal1(C, C.length));
 	}
 
-    public static int[] mergeTwoSortedArrays(int[] a, int[]b) {
-
-		int[] sorted = new int[a.length + b.length];
-
-		int i = 0;
-		int j = 0;
-		int k = 0;
-
-		while (i < a.length && j < b.length) {
-
-			if (a[i] < b[j]) {
-				sorted[k] = a[i];
-				k++;
-				i++;
-			} else {
-				sorted[k] = b[j];
-				k++;
-				j++;
-			}
+	public static long maxVal1(long A[], int n) {
+		long max = A[0];
+		for (int i = 1; i < n; i++) {
+			if (A[i] > max)
+				max = A[i];
 		}
-
-		if (i == a.length) {
-
-			while (j < b.length) {
-				sorted[k] = b[j];
-				k++;
-				j++;
-			}
-		}
-
-		if (j == b.length) {
-
-			while (i < a.length) {
-				sorted[k] = a[i];
-				k++;
-				i++;
-			}
-		}
-
-		return sorted;
-
+		return max;
 	}
 
-	public static int[] mergeSort(int[] arr, int lo, int hi) {
+	public static long[] geraVetor(int nroPares, int nroImpares) {
+		long[] res = null;
+		int contPar = 0, contImpar = 0, novoNum;
+		Random rnd = new Random();
 
-		if (lo == hi) {
-			int[] br = new int[1];
-			br[0] = arr[lo];
+		if ((nroPares >= 0) ||
+				(nroImpares >= 0) &&
+						(nroPares + nroImpares > 0)) {
 
-			return br;
+			res = new long[nroPares + nroImpares];
+
+			while ((contPar < nroPares) || (contImpar < nroImpares)) {
+				novoNum = rnd.nextInt(98) + 1;
+
+				if ((novoNum % 2 == 0) && (contPar < nroPares)) {
+					res[contPar + contImpar] = novoNum;
+					contPar++;
+				} else if ((novoNum % 2 == 1) && (contImpar < nroImpares)) {
+					res[contPar + contImpar] = novoNum;
+					contImpar++;
+				}
+			}
 		}
 
-		int mid = (lo + hi) / 2;
-
-		int[] fh = mergeSort(arr, lo, mid);
-		int[] sh = mergeSort(arr, mid + 1, hi);
-
-		int[] merged = mergeTwoSortedArrays(fh, sh);
-
-		return merged;
+		return res;
 	}
 }
